0685edf7b0e488b19b99de1b36919e2efb50ec84
Jerald Thomas <jerald@jeraldthomas.com>
2022-03-30 21:12:25 -0400

    still working on logging analytics

--- jeraldthomas_dot_com/analytics.py
+++ jeraldthomas_dot_com/analytics.py
@@ -1,10 +1,15 @@
 from functools import wraps
 from flask import g, request, redirect, url_for
+from tinydb import TinyDB
+import time
 
 def log_views(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        print("Logging:", request.url)
-        return f(args, kwargs)
-        
+        db = TinyDB('views.json')
+        db.insert({'time': time.time(),
+                  'endpoint': request.endpoint,
+                  'ip': request.remote_addr})
+        return f(*args, **kwargs)
+
     return decorated_function
